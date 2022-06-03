faf076795ec8341c3689f8c61a3458485d010dcf
Court <SCO941@multihosp.net>
2022-06-03 16:05:15 -0400

    Continuing chapter 1

---
+++ Chapter 1/objectLiteralSyntax.js
@@ -0,0 +1,25 @@
+// An object literal can have a trailing comma. This makes it easy to add other properties as the code evolves.
+
+let harry = {
+	name: "Harry Smith",
+	age: 42,
+}
+
+// When declaring an object literal, property values are stored in variables whose names are equal to the property names.
+let age = 43
+harry = {
+	name: "Harry Smith",
+	age, // This is a shortcut for "age: age".
+}
+
+// Use brackets for the computed property names in object literals.
+let field = "Age"
+harry = {
+	name: "Harry Smith",
+	[field.toLowerCase()]: 42,
+}
+
+// A property name is alwasy a string. If the name doesn't follow the rules of an identifier, quote it in an object literal.
+harry = {
+	name: "Harry Smith",
+}
