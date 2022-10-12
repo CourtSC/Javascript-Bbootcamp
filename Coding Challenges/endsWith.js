91484296bb569ae1f0db652b97cc98d9c64c182d
Scott Court <scott.c.court@gmail.com>
2022-10-12 16:14:22 -0400

    More challenges

---
+++ Coding Challenges/endsWith.js
@@ -0,0 +1,17 @@
+/* 
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
+
+/* 
+const solution = (str, ending) => {
+	const idxStart = str.length - ending.length;
+	return str.slice(idxStart, str.length) === ending;
+};
+*/
+
+const solution = (str, ending) => str.endsWith(ending);
